b4e277513342afe32bba5c4396863116587050e0
lvqh12@gmail.com <lvqh12@gmail.com@7ff576d3-8f4c-a098-20e3-ee69b0eae82b>
2013-12-06 12:50:34 +0000

    them phan trang

---
+++ trunk/application/modules/admin/bootstrap.php
@@ -0,0 +1,15 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of bootstrap
+ *
+ * @author Mr
+ */
+class Admin_Bootstrap extends Zend_Application_Module_Bootstrap{ 
+   
+}
