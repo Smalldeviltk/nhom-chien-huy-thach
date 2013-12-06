560ad29f9303c39a6897503aba2e3ef0a654f75e
lvqh12@gmail.com <lvqh12@gmail.com@7ff576d3-8f4c-a098-20e3-ee69b0eae82b>
2013-12-06 06:13:19 +0000

    modules admin

--- trunk/application/modules/admin/controllers/ThiepController.php
+++ trunk/application/modules/admin/controllers/ThiepController.php
@@ -10,8 +10,25 @@
  *
  * @author Mr
  */
-class ThiepController {
+class Admin_ThiepController extends Zend_Controller_Action {
+
     //put your code here
+    private $thiepTable;
+
+    public function init() {
+        $this->thiepTable = new Application_Model_DbTable_Thiep();
+    }
+
+    public function indexAction() {
+        if (isset($_GET['ct']) && !empty($_GET['id'])) {//sua
+            $thiepSelect = $this->thiepTable->listThiep("id = '" . $_GET['id'] . "'");
+        } else {
+            $thiepSelect = $this->thiepTable->listThiep();
+        }
+
+        $this->view->thieps = $thiepSelect;
+    }
+
 }
 
 ?>
