69c4e873193cbfe28982efea2e139dccd516d10e
lvqh12 <lvqh12@gmail.com>
2013-12-07 08:16:30 +0000

    default bootstrap

---
+++ application/modules/default/bootstrap.php
@@ -0,0 +1,15 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of bootstrap
+ *
+ * @author Mr
+ */
+class Default_Bootstrap extends Zend_Application_Module_Bootstrap{ 
+   
+}
