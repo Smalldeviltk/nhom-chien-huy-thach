498c2be269260e45b2e40d39ffdc693476b78f09
lvqh12 <lvqh12@gmail.com>
2013-12-07 14:33:44 +0000

    layout

--- cnweb.sql
+++ cnweb.sql
@@ -3,9 +3,9 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 03, 2013 at 06:08 PM
+-- Generation Time: Dec 07, 2013 at 11:16 AM
 -- Server version: 5.6.11
--- PHP Version: 5.5.3
+-- PHP Version: 5.5.1
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -30,21 +30,50 @@ USE `cnweb`;
 
 CREATE TABLE IF NOT EXISTS `thiep` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
+  `hinhanh` text,
   `masanpham` text NOT NULL,
   `tensanpham` text NOT NULL,
   `thongtin` text,
   `gia` text,
-  `hinhanh` text,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+  PRIMARY KEY (`id`),
+  FULLTEXT KEY `HinhAnh` (`hinhanh`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=6 ;
 
 --
 -- Dumping data for table `thiep`
 --
 
-INSERT INTO `thiep` (`masanpham`, `tensanpham`, `thongtin`, `gia`, `hinhanh`) VALUES
-('1', 'Kim Binh Mai', 'kim-binh-mai', '50000', '1.jpg');
-('2', 'Nhuc Bo Doan', 'nhuc-bo-doan', '50000', '2.jpg');
+INSERT INTO `thiep` (`id`, `hinhanh`, `masanpham`, `tensanpham`, `thongtin`, `gia`) VALUES
+(1, 'MAU_1.jpg', '1', 'Nhuc Bo Doan', 'NBD', '5000'),
+(2, '1.jpg', '2', 'Kim Binh Mai', 'kim-binh-mai', '50000'),
+(3, 'MAU_1.jpg', '1', 'Kim Binh Mai', 'kim-binh-mai', '50000'),
+(4, 'MAU_1.jpg', '1', 'Kim Binh Mai', 'kim-binh-mai', '50000'),
+(5, 'MAU_1.jpg', '1', 'clgt', 'ádsadsadasda', '5000');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE IF NOT EXISTS `user` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `username` varchar(50) NOT NULL,
+  `password` char(32) NOT NULL,
+  `level` int(1) NOT NULL DEFAULT '1',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=6 ;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`id`, `username`, `password`, `level`) VALUES
+(1, 'admin', '12345', 2),
+(2, 'admin', '12345', 2),
+(3, 'kenny', '12345', 2),
+(4, 'jacky', '12345', 1),
+(5, 'Lena', '12345', 1);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
