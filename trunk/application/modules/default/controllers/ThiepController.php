b546a8b1c931cd893542df045669c2f7b915b406
lvqh12@gmail.com <lvqh12@gmail.com@7ff576d3-8f4c-a098-20e3-ee69b0eae82b>
2013-12-08 09:31:40 +0000

    them module default.

--- trunk/application/modules/default/controllers/ThiepController.php
+++ trunk/application/modules/default/controllers/ThiepController.php
@@ -13,26 +13,26 @@
 class ThiepController extends Zend_Controller_Action {
 
     //put your code here
-    //private $thiepTable;
+    private $thiepTable;
 
     public function init() {
-        //$this->thiepTable = new Model_Thiep();
+        $this->thiepTable = new Admin_Model_Thiep();
     }
 
     public function indexAction() {
-//        $adapter = new Zend_Paginator_Adapter_DbSelect($this->thiepTable->listAllThiep());
-//        $paginator = new Zend_Paginator($adapter);
-//        $paginator->setItemCountPerPage(9);
-//        $paginator->setPageRange(5);
-//        $currentPage = $this->_request->getParam('page', 1);
-//        $paginator->setCurrentPageNumber($currentPage);
-//        $this->view->data = $paginator;
+        $adapter = new Zend_Paginator_Adapter_DbSelect($this->thiepTable->listAllThiep());
+        $paginator = new Zend_Paginator($adapter);
+        $paginator->setItemCountPerPage(9);
+        $paginator->setPageRange(5);
+        $currentPage = $this->_request->getParam('page', 1);
+        $paginator->setCurrentPageNumber($currentPage);
+        $this->view->data = $paginator;
     }
     
     public function detailAction() {
-//        if (isset($_GET['ctsp']) && !empty($_GET['id'])) {
-//            $this->view->detail = $this->thiepTable->fetchThiep($_GET['id'] );
-//        }
+        if (isset($_GET['ctsp']) && !empty($_GET['id'])) {
+            $this->view->detail = $this->thiepTable->fetchThiep($_GET['id'] );
+        }
     }
 
 }
